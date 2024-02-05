94eef3b49f02fabdd553070897c855da602810fb
Stanley-Ugo <iamkosmicblast@gmail.com>
2024-02-05 21:48:05 +0100

    Adding POST PUT & DELETE Routes

--- Catalog/Controllers/ItemsController.cs
+++ Catalog/Controllers/ItemsController.cs
@@ -34,6 +34,55 @@ namespace Catalog.Controllers
             if(item is null)
                return NotFound();
             return Ok(item.AsDTO());
-        } 
+        }
+
+        [HttpPost]
+        public ActionResult<ItemDTO> CreateItem(CreateItemDTO createItemDTO)
+        {
+            Item item = new()
+            {
+                Id = Guid.NewGuid(),
+                Name = createItemDTO.Name,
+                Price = createItemDTO.Price,
+                DateCreated = DateTimeOffset.UtcNow,
+            };
+
+            _inMemoryRepository.CreateItem(item);
+
+            return CreatedAtAction(nameof(GetItem), new {id = item.Id}, item.AsDTO());
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateItem(Guid id, UpdateItemDTO updateItemDTO)
+        {
+            var existingItem = _inMemoryRepository.GetItem(id);
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+            Item updatedItem = existingItem with
+            {
+                Name = updateItemDTO.Name,
+                Price = updateItemDTO.Price
+            };
+
+            _inMemoryRepository.UpdateItem(updatedItem);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteItem(Guid id)
+        {
+            var existingItem = _inMemoryRepository.GetItem(id);
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+
+            _inMemoryRepository.DeleteItem(id);
+
+            return NoContent();
+        }
     }
 }
